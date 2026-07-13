2695153985d41f54be6250eed0856b79f0a1e745
Vladimir <vladimir@what.digital>
2026-07-13 19:32:42 +0400

    add alembic

---
+++ core/settings.py
@@ -0,0 +1,26 @@
+import os
+
+from pydantic_settings import BaseSettings, SettingsConfigDict
+
+
+class Settings(BaseSettings):
+    API_KEY: str = "123"
+    LOG_LEVEL: str = "error"
+    DEBUG: bool = False
+
+    DATABASE_URL: str = "postgresql+asyncpg://postgres:postgres@0.0.0.0:5432/postgres"
+    SYNC_DATABASE_URL: str = "postgresql+psycopg2://postgres:postgres@0.0.0.0:5432/postgres"
+
+    HOST: str = "0.0.0.0"
+    PORT: int = 8000
+
+
+    model_config = SettingsConfigDict(
+        env_file=os.getenv("ENV_FILE", ".env"),
+        env_file_encoding="utf-8",
+        extra="ignore",
+        case_sensitive=True
+    )
+
+
+settings = Settings()
