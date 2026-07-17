be273e0441af75586587fdfa909695efcc4a3744
Vladimir <vladimir@what.digital>
2026-07-17 15:33:32 +0400

    fix deps

--- src/payments/dependencies.py
+++ src/payments/dependencies.py
@@ -2,6 +2,7 @@ from typing import Annotated
 
 from fastapi import Depends, Header, HTTPException
 from sqlalchemy.ext.asyncio import AsyncSession
+from fastapi import status
 
 from core.database import get_session
 from core.settings import settings
@@ -12,7 +13,7 @@ from payments.services.payment import PaymentService
 
 async def verify_api_key(x_api_key: str = Header(...)):
     if x_api_key != settings.API_KEY:
-        raise HTTPException(status_code=403, detail="Invalid API Key")
+        raise HTTPException(status_code=status.HTTP_403_FORBIDDEN, detail="Invalid API Key")
 
 
 async def get_payment_service(session: Annotated[AsyncSession, Depends(get_session)]) -> PaymentService:
